acdba6e6ae33681d8585857d51b6d389f1f83461
bnsm98 <91394720+bnsm98@users.noreply.github.com>
2022-11-30 10:19:45 -0500

    Ejercicios secuenciales en JavaScript

---
+++ secuenciales.js
@@ -0,0 +1,39 @@
+let a=2,b=5,c=1
+
+console.log("Point 1")
+console.log("a. ", 3*a-4*b/(a**2))
+console.log("b. ",(((b+c)/2*a+10)*3*b)-6)
+
+console.log("Point 2")
+let capital=2000000
+console.log("Profits: ",capital*0.012)
+
+console.log("Point 3")
+let height=5,width=4
+console.log("Area: ", (height*width)/2)
+
+console.log("Point 4")
+let salary = 980000, commission = 170000, nCars = 3, totalSales = 90000000
+console.log("Total Salary: ", salary + (nCars*commission) + (totalSales*0.05))
+
+console.log("Point 5")
+let grade1 = 4.5, grade2=4.6, grade3=3.5, grade4=4.0
+console.log("Final Grade: ", ((grade1+grade2)/2)*0.4 + ((grade3+grade4)/2)*0.6)
+
+console.log("Point 6")
+let age = 24
+console.log("bpm: ",(220-age)/10)
+
+console.log("Point 7")
+let salaryBase = 1000000, sale1 = 700000, sale2=2000000, sale3=400000
+let sales = (sale1+sale2+sale3)
+console.log("Total for commissions: ", sales*0.1)
+console.log("Total Salary: ", salaryBase + sales*0.1)
+
+console.log("Point 8")
+let mathExam=4.0, mathGrade1=3.9, mathGrade2=3.8, mathGrade3=4.2
+let physicsExam=3.8, physicsGrade1=4.8, physicsGrade2=4.5
+let chemistryExam=3.2, chemistryGrade1=4.2, chemistryGrade2=4.3, chemistryGrade3=4.4
+console.log("Final Grade Math: ", mathExam*0.9 + ((mathGrade1+mathGrade2+mathGrade3)/3)*0.1)
+console.log("Final Grade Physics: ", physicsExam*0.8 + ((physicsGrade1+physicsGrade2)/2)*0.2)
+console.log("Final Grade Chemistry: ", chemistryExam*0.85 + ((chemistryGrade1+chemistryGrade2+chemistryGrade3)/3)*0.15)
